d0901d9606d9334a22018b71982649bb28abe2c0
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-13 12:25:35 -0400

    added pixel

--- client/src/pages/ThanksQ/ThanksQ.js
+++ client/src/pages/ThanksQ/ThanksQ.js
@@ -25,6 +25,9 @@ const ThanksQ = () => {
           ))}
         </div>
       </div>
+      {/* Offer Conversion: RateTrooper - Affordable Auto Insurance Quotes - US */}
+      <iframe title='pixel' src="https://offers.axtracking.com/aff_l?offer_id=1269" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+      {/* End Offer Conversion */}
     </div>
   )
 }
