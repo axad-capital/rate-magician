440aca342d05d5b06421ea5f259ba67f4080c8d6
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-18 16:24:43 -0400

    changed quote form to have less input fields

--- client/src/components/QuoteForm/QuoteForm.js
+++ client/src/components/QuoteForm/QuoteForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './quoteForm.css'
+import CarLogoQuote from './header-img.png'
 
 const QuoteForm = () => {
 
@@ -16,13 +17,13 @@ const QuoteForm = () => {
             .then(data => {
                 let autoData = {
                     id: uuidv4(),
-                    firstName: document.getElementById('first').value,
-                    lastName: document.getElementById('last').value,
-                    email: document.getElementById('email').value,
+                    // firstName: document.getElementById('first').value,
+                    // lastName: document.getElementById('last').value,
+                    // email: document.getElementById('email').value,
                     homeOwner: document.getElementById('home-owner').value,
                     insured: document.getElementById('insured').value,
                     vehicles: document.getElementById('vehicles').value,
-                    phone: document.getElementById('phone-num').value,
+                    // phone: document.getElementById('phone-num').value,
                     zipcode: document.getElementById('auto-zip').value
                 }
 
@@ -37,9 +38,9 @@ const QuoteForm = () => {
                         "ni_ref": "https://www.ratemagician.com/thank-you"
                     },
                     "contact": {
-                        "first_name": autoData.firstName,
-                        "last_name": autoData.lastName,
-                        "email": autoData.email,
+                        // "first_name": autoData.firstName,
+                        // "last_name": autoData.lastName,
+                        // "email": autoData.email,
                         "zip": autoData.zipcode,
                         "homeowner": autoData.homeOwner,
                     },
@@ -72,47 +73,52 @@ const QuoteForm = () => {
     }
     return (
         <div>
-            <div className='quote-form'>
-                <h1 id='quote-heading'>Get A Free Quote Now</h1>
-                <p className='quote-sub-heading'>Stay Home & Find Affordable Car Insurance In 2 Minutes</p>
-                <div className='auto-form'>
-                    <label htmlFor="first">First Name</label>
+            <div className='quote-main-container'>
+                <div className='quote-main-img'>
+                    <img className='quote-img' src={CarLogoQuote} alt="car" />
+                </div>
+                <div className='quote-form'>
+                    <h1 id='quote-heading'>Get A Free Quote Now</h1>
+                    <p className='quote-sub-heading'>Stay Home & Find Affordable Car Insurance In 2 Minutes</p>
+                    <div className='auto-form'>
+                        {/* <label htmlFor="first">First Name</label>
                     <input id='first' name='first' className='form-input' type="text" placeholder='First Name' />
                     <label htmlFor="last">Last Name</label>
-                    <input id='last' name='last' className='form-input' type="text" placeholder='Last Name' />
+                    <input id='last' name='last' className='form-input' type="text" placeholder='Last Name' /> */}
 
-                    <label className='zip-code-label' htmlFor="zip">Enter Your Zipcode</label>
-                    <input id='auto-zip' name='zip' className='form-input' type="text" placeholder='Zipcode' />
+                        <label className='zip-code-label' htmlFor="zip">Enter Your Zipcode</label>
+                        <input id='auto-zip' name='zip' className='form-input' type="text" placeholder='Zipcode' />
 
-                    <label htmlFor="email">Email</label>
-                    <input id='email' name='email' className='form-input' type="text" placeholder='Email' />
+                        {/* <label htmlFor="email">Email</label>
+                    <input id='email' name='email' className='form-input' type="text" placeholder='Email' /> */}
 
-                    {/* <label htmlFor="bday">Date of Birth</label>
+                        {/* <label htmlFor="bday">Date of Birth</label>
                     <input id='bday' name='bday' className='form-input' type="date" /> */}
 
-                    <label htmlFor="home-owner">Are you a Home Owner?</label>
-                    <select id='home-owner' className='form-input' name="home-owner">
-                        <option value="1">Yes</option>
-                        <option value="0">No</option>
-                    </select>
-                    <label htmlFor="insured">Are you currently insured?</label>
-                    <select id='insured' className='form-input' name="insured">
-                        <option value="Yes">Yes</option>
-                        <option value="No">No</option>
-                    </select>
+                        <label htmlFor="home-owner">Are you a Home Owner?</label>
+                        <select id='home-owner' className='form-input' name="home-owner">
+                            <option value="1">Yes</option>
+                            <option value="0">No</option>
+                        </select>
+                        <label htmlFor="insured">Are you currently insured?</label>
+                        <select id='insured' className='form-input' name="insured">
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </select>
 
-                    <label htmlFor="vehicles">How Many Vehicles Do You Own</label>
-                    <input id='vehicles' name='vehicles' className='form-input' type="number" placeholder='Vehicle Count' />
+                        <label htmlFor="vehicles">How Many Vehicles Do You Own</label>
+                        <input id='vehicles' name='vehicles' className='form-input' type="number" placeholder='Vehicle Count' />
 
-                    <label htmlFor="phone-num">Phone Number</label>
-                    <input id='phone-num' name='phone-num' className='form-input' type="text" placeholder='Phone Number' />
+                        {/* <label htmlFor="phone-num">Phone Number</label>
+                    <input id='phone-num' name='phone-num' className='form-input' type="text" placeholder='Phone Number' /> */}
 
-                    <br />
-                    <button className='form-btn' onClick={quoteFormHandler}>Submit</button>
-                    <br />
-                    {/* <p style={{ color: 'red', textAlign: 'center' }} >{blankHandler}</p> */}
+                        <br />
+                        <button className='form-btn' onClick={quoteFormHandler}>Submit</button>
+                        <br />
+                        {/* <p style={{ color: 'red', textAlign: 'center' }} >{blankHandler}</p> */}
+                    </div>
+                    <p className='permission'>By clicking "Submit", I provide my express written consent via electronic signature authorizing Rate Magician and one or more Auto Insurance specialists, their agents and marketing partners to contact me and other related products and services to the number and email address I provided (including any wireless number). I further expressly consent to receive telemarketing emails, calls, text messages, pre-recorded messages, and artificial voice messages via an autodialed phone system, even if my telephone number is a mobile number that is currently listed on any state, federal or corporate “Do Not Call” list. I understand that my consent is not a condition of purchase of any goods or services and that standard message and data rates may apply.</p>
                 </div>
-                <p className='permission'>By clicking "Submit", I provide my express written consent via electronic signature authorizing Rate Magician and one or more Auto Insurance specialists, their agents and marketing partners to contact me and other related products and services to the number and email address I provided (including any wireless number). I further expressly consent to receive telemarketing emails, calls, text messages, pre-recorded messages, and artificial voice messages via an autodialed phone system, even if my telephone number is a mobile number that is currently listed on any state, federal or corporate “Do Not Call” list. I understand that my consent is not a condition of purchase of any goods or services and that standard message and data rates may apply.</p>
             </div>
         </div>
     )
