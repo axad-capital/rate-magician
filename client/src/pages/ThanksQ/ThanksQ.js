366cc30996934ab2aace9ee0922951e500b6699f
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-22 11:10:52 -0400

    added old pixel

--- client/src/pages/ThanksQ/ThanksQ.js
+++ client/src/pages/ThanksQ/ThanksQ.js
@@ -14,6 +14,7 @@ const ThanksQ = () => {
         <h1 className='ad-title'>Sorry! There are currently no offers for your area at this time, try a different zipcode or try again later.</h1>
         <button onClick={() => window.location.href = '/get-quote'} className='back-btn'>Back</button>
         {/* Offer Conversion: RateTrooper - Affordable Auto Insurance Quotes - US */}
+        <iframe title='pixel' src="https://offers.axtracking.com/aff_l?offer_id=1269" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1269&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         {/* End Offer Conversion */}
       </div>
@@ -38,6 +39,7 @@ const ThanksQ = () => {
           </div>
         </div>
         {/* Offer Conversion: RateTrooper - Affordable Auto Insurance Quotes - US */}
+        <iframe title='pixel' src="https://offers.axtracking.com/aff_l?offer_id=1269" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1269&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         {/* End Offer Conversion */}
       </div>
